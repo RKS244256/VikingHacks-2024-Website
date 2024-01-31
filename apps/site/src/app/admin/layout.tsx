8288dcca403685cc4b6a7bf84bd0e9b137297739
Ayaan <76827380+AyaanT0@users.noreply.github.com>
2024-01-31 18:09:07 -0500

    g

--- apps/site/src/app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next/types";
-
-import "@cloudscape-design/global-styles/index.css";
-
-export const metadata: Metadata = {
-	title: "Admin | IrvineHacks 2024",
-};
-
-export { default as default } from "./layout/AdminLayout";
