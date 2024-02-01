523adfa08a7d4b8c4ce6ce90baa3f18eff3c94bb
Ayaan <76827380+AyaanT0@users.noreply.github.com>
2024-01-31 19:16:03 -0500

    sssd

---
+++ apps/api/src/routers/user.py
@@ -0,0 +1,154 @@
+from datetime import datetime, timezone
+from logging import getLogger
+from typing import Annotated, Optional, Union
+
+from fastapi import APIRouter, Depends, Form, HTTPException, UploadFile, status
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel, EmailStr
+
+from auth import user_identity
+from auth.user_identity import User, require_user_identity, use_user_identity
+from models.ApplicationData import ProcessedApplicationData, RawApplicationData
+from services import mongodb_handler
+from services.mongodb_handler import Collection
+from utils import email_handler, resume_handler
+from utils.user_record import Applicant, Role, Status
+
+log = getLogger(__name__)
+
+router = APIRouter()
+
+DEADLINE = datetime(2024, 1, 15, 7, 59, tzinfo=timezone.utc)
+
+
+class IdentityResponse(BaseModel):
+    uid: Union[str, None] = None
+    status: Union[str, None] = None
+    role: Union[Role, None] = None
+
+
+def _is_past_deadline(now: datetime) -> bool:
+    return now > DEADLINE
+
+
+@router.post("/login")
+async def login(email: EmailStr = Form()) -> RedirectResponse:
+    log.info("%s requested to log in", email)
+    if user_identity.uci_email(email):
+        # redirect user to UCI SSO login endpoint, changing to GET method
+        return RedirectResponse("/api/saml/login", status.HTTP_303_SEE_OTHER)
+    return RedirectResponse("/api/guest/login", status.HTTP_307_TEMPORARY_REDIRECT)
+
+
+@router.get("/logout")
+async def logout() -> RedirectResponse:
+    """Clear user identity cookie."""
+    response = RedirectResponse("/", status.HTTP_303_SEE_OTHER)
+    user_identity.remove_user_identity(response)
+    return response
+
+
+@router.get("/me", response_model=IdentityResponse)
+async def me(
+    user: Annotated[Union[User, None], Depends(use_user_identity)]
+) -> dict[str, object]:
+    log.info(user)
+    if not user:
+        return dict()
+    user_record = await mongodb_handler.retrieve_one(
+        Collection.USERS, {"_id": user.uid}
+    )
+    if not user_record:
+        return {"uid": user.uid}
+    return {**user_record, "uid": user.uid}
+
+
+@router.post("/apply", status_code=status.HTTP_201_CREATED)
+async def apply(
+    user: Annotated[User, Depends(require_user_identity)],
+    raw_application_data: Annotated[RawApplicationData, Depends(RawApplicationData)],
+    resume: Optional[UploadFile] = None,
+) -> str:
+    # Check if current datetime is past application deadline
+    now = datetime.now(timezone.utc)
+
+    if _is_past_deadline(now):
+        raise HTTPException(status.HTTP_403_FORBIDDEN, "Applications have closed.")
+
+    # check if email is already in database
+    EXISTING_RECORD = await mongodb_handler.retrieve_one(
+        Collection.USERS, {"_id": user.uid}
+    )
+
+    if EXISTING_RECORD and "status" in EXISTING_RECORD:
+        log.error("User %s has already applied.", user)
+        raise HTTPException(status.HTTP_400_BAD_REQUEST)
+
+    raw_app_data_dump = raw_application_data.model_dump()
+
+    for field in ["pronouns", "ethnicity", "school", "major"]:
+        if raw_app_data_dump[field] == "other":
+            raise HTTPException(
+                status.HTTP_422_UNPROCESSABLE_ENTITY,
+                "Please enable JavaScript on your browser.",
+            )
+
+    if resume is not None and resume.size and resume.size > 0:
+        try:
+            resume_url = await resume_handler.upload_resume(
+                raw_application_data, resume
+            )
+        except TypeError:
+            log.info("%s provided invalid resume type.", user)
+            raise HTTPException(
+                status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, "Invalid resume file type"
+            )
+        except ValueError:
+            log.info("%s provided too large resume.", user)
+            raise HTTPException(
+                status.HTTP_413_REQUEST_ENTITY_TOO_LARGE, "Resume upload is too large"
+            )
+        except RuntimeError as err:
+            log.error("During user %s apply, resume upload: %s", user.uid, err)
+            raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        resume_url = None
+
+    processed_application_data = ProcessedApplicationData(
+        **raw_app_data_dump,
+        resume_url=resume_url,
+        submission_time=now,
+    )
+    applicant = Applicant(
+        uid=user.uid,
+        application_data=processed_application_data,
+        status=Status.PENDING_REVIEW,
+    )
+
+    # add applicant to database
+    try:
+        await mongodb_handler.update_one(
+            Collection.USERS,
+            {"_id": user.uid},
+            applicant.model_dump(),
+            upsert=True,
+        )
+    except RuntimeError:
+        log.error("Could not insert applicant %s to MongoDB.", user.uid)
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    try:
+        await email_handler.send_application_confirmation_email(
+            user.email, applicant.application_data
+        )
+    except RuntimeError:
+        log.error("Could not send confirmation email with SendGrid to %s.", user.uid)
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    # TODO: handle inconsistent results if one service fails
+
+    log.info("%s submitted an application", user.uid)
+    return (
+        "Thank you for submitting an application to IrvineHacks 2024! Please "
+        + "visit https://irvinehacks.com/portal to see your application status."
+    )
---
+++ apps/api/src/services/sendgrid_handler.py
@@ -0,0 +1,116 @@
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import os
+from enum import Enum
+from logging import getLogger
+from typing import Iterable, Literal, Tuple, TypedDict, Union, overload
+
+import aiosendgrid
+from httpx import HTTPStatusError
+from sendgrid.helpers.mail import Email, Mail, Personalization
+
+log = getLogger(__name__)
+
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+
+
+class Template(str, Enum):
+    CONFIRMATION_EMAIL = "d-83d42cc17b54456183eeb946ba58861a"
+    GUEST_TOKEN = "d-1998e588ddf74c6d9ede36b778730176"
+
+
+class PersonalizationData(TypedDict):
+    email: str
+
+
+class ConfirmationPersonalization(PersonalizationData):
+    first_name: str
+    last_name: str
+
+
+class GuestTokenPersonalization(PersonalizationData):
+    passphrase: str
+
+
+@overload
+async def send_email(
+    template_id: Literal[Template.CONFIRMATION_EMAIL],
+    sender_email: Tuple[str, str],
+    receiver_data: ConfirmationPersonalization,
+    send_to_multiple: Literal[False] = False,
+    reply_to: Union[Tuple[str, str], None] = None,
+) -> None:
+    ...
+
+
+@overload
+async def send_email(
+    template_id: Literal[Template.GUEST_TOKEN],
+    sender_email: Tuple[str, str],
+    receiver_data: GuestTokenPersonalization,
+    send_to_multiple: Literal[False] = False,
+    reply_to: Union[Tuple[str, str], None] = None,
+) -> None:
+    ...
+
+
+@overload
+async def send_email(
+    template_id: Literal[Template.CONFIRMATION_EMAIL],
+    sender_email: Tuple[str, str],
+    receiver_data: Iterable[ConfirmationPersonalization],
+    send_to_multiple: Literal[True],
+    reply_to: Union[Tuple[str, str], None] = None,
+) -> None:
+    ...
+
+
+async def send_email(
+    template_id: Template,
+    sender_email: Tuple[str, str],
+    receiver_data: Union[PersonalizationData, Iterable[PersonalizationData]],
+    send_to_multiple: bool = False,
+    reply_to: Union[Tuple[str, str], None] = None,
+) -> None:
+    """
+    Send a personalized templated email to one or multiple receivers via SendGrid
+    """
+    try:
+        email_message = Mail()
+
+        if send_to_multiple:
+            if isinstance(receiver_data, dict):
+                raise TypeError(
+                    f"Expected {list} for receiver_data but got {type(receiver_data)}"
+                )
+            for r in receiver_data:
+                p = Personalization()
+                p.add_to(Email(email=r["email"], dynamic_template_data=r))
+                email_message.add_personalization(p)
+        else:
+            if not isinstance(receiver_data, dict):
+                raise TypeError(
+                    f"Expected {dict} for receiver_data but got {type(receiver_data)}"
+                )
+            p = Personalization()
+            p.add_to(
+                Email(
+                    email=receiver_data["email"],
+                    dynamic_template_data=receiver_data,
+                )
+            )
+            email_message.add_personalization(p)
+
+        if reply_to is not None:
+            email_message.reply_to = reply_to
+
+        email_message.from_email = sender_email
+        email_message.template_id = template_id
+
+        async with aiosendgrid.AsyncSendGridClient(api_key=SENDGRID_API_KEY) as client:
+            response = await client.send_mail_v3(body=email_message.get())
+            log.debug(response.status_code)
+            log.debug(response.headers)
+    except HTTPStatusError as e:
+        log.exception("During SendGrid processing: %s", e)
+        raise RuntimeError("Could not send email with SendGrid")
